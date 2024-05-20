ba2455bbb56291db8b0d3de9a4083ee4cb50cc59
vlikcc <vli.kcc@hotmail.com>
2024-05-20 13:53:30 +0300

    20.05.2024

---
+++ guneshukuk.DataAccessLayer/Migrations/20240520105234_Booking.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace guneshukuk.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Booking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BookingDates",
+                table: "BookingDates",
+                newName: "AvailableDates");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AvailableDates",
+                table: "BookingDates",
+                newName: "BookingDates");
+        }
+    }
+}
