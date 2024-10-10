b47b160b9edc907ec83b0e5c25c55b072cde8edb
vlikcc <vli.kcc@hotmail.com>
2024-10-10 14:42:48 +0300

    10.10.2024

--- guneshukuk.WebAPI/Program.cs
+++ guneshukuk.WebAPI/Program.cs
@@ -3,7 +3,6 @@ using guneshukuk.BusinessLayer.Concrete;
 using guneshukuk.DataAccessLayer.Abstract;
 using guneshukuk.DataAccessLayer.Concrete;
 using guneshukuk.DataAccessLayer.EntitiyFramework;
-using guneshukuk.EntityLayer.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
@@ -11,12 +10,11 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Service registration
 builder.Services.AddDbContext<GuneshukukContext>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
-
-builder.Services.AddScoped<IEmailService,EmailManager>();
-
+builder.Services.AddScoped<IEmailService, EmailManager>();
 builder.Services.AddScoped<IAboutService, AboutManager>();
 builder.Services.AddScoped<IAboutDal, EfAboutDal>();
 
@@ -35,18 +33,19 @@ builder.Services.AddScoped<IConsultancyDal, EfConsultancyDal>();
 builder.Services.AddScoped<ISocialMediaService, SocialMediaManager>();
 builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
 
-builder.Services.AddScoped<IBookingDateService,BookingDateManager>();
-builder.Services.AddScoped<IBookingDateDal,EfBoookingDateDal>();
+builder.Services.AddScoped<IBookingDateService, BookingDateManager>();
+builder.Services.AddScoped<IBookingDateDal, EfBoookingDateDal>();
 
-builder.Services.AddScoped<IBookingTimeService,BookingTimeManager>();   
-builder.Services.AddScoped<IBookingTimeDal, EfBookingTimeDal>();    
-
-
+builder.Services.AddScoped<IBookingTimeService, BookingTimeManager>();
+builder.Services.AddScoped<IBookingTimeDal, EfBookingTimeDal>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c=>c.SwaggerDoc("V1",new OpenApiInfo { Title="AvukatSerminGunes",Version="V1"}));
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AvukatSerminGunes", Version = "v1" });
+});
+
 builder.Services.AddCors();
 builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
 {
@@ -56,7 +55,6 @@ builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsigh
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -67,12 +65,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors(builder => builder.WithOrigins("https://localhost:7108/").AllowAnyHeader());
-
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
