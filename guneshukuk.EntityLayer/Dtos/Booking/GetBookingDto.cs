0d3fa7a7b203bb77da6da95f047c82116557bf50
vlikcc <vli.kcc@hotmail.com>
2024-11-25 11:35:01 +0300

    25.11.2024-2

--- guneshukuk.EntityLayer/Dtos/Booking/GetBookingDto.cs
+++ guneshukuk.EntityLayer/Dtos/Booking/GetBookingDto.cs
@@ -1,4 +1,5 @@
-﻿using guneshukuk.EntityLayer.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using guneshukuk.EntityLayer.Entities;
 
 namespace guneshukuk.EntityLayer.Dtos.Booking
 {
@@ -9,6 +10,10 @@ namespace guneshukuk.EntityLayer.Dtos.Booking
         public string BookingEmail { get; set; }
         public string BookingPhone { get; set; }
         public string BookingMessage { get; set; }
-        public BookingDate BookingDate { get; set; }
+
+        [ForeignKey("BookingDateId")]
+        public int BookingDateId { get; set; }
+
+        public BookingDate? BookingDate { get; set; }
     }
 }
